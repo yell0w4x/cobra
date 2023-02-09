487bb368e32ae2125e8f51da557e04fa8b34eb11
yell0w4x <yellow4x@gmail.com>
2023-02-10 00:03:10 +0200

    Add e2e tests base

---
+++ e2e-tests/tests/src/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture
+def run():
+    pass
+
+
+@pytest.fixture
+def containers():
+    pass
+
+
+@pytest.fixture
+def volumes():
+    pass
---
+++ e2e-tests/tests/src/share.py
@@ -0,0 +1,5 @@
+import docker
+
+
+def run():
+    pass
